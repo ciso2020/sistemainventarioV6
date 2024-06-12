537a3d1ae9faaa0d59d17b1ce016db9d53a73d0d
ciso2020 <cisoivan2020@gmail.com>
2024-06-12 02:22:40 -0600

    first commit

---
+++ sistemainventarioV6/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace sistemainventarioV6.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
